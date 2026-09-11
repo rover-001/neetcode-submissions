a0a190a7bc32f6a24b004706f1c8bcc4352f4fe9
Rover <110387638+rover-001@users.noreply.github.com>
2026-09-11 19:18:07 +0530

    Add: sum-of-all-subset-xor-totals - submission-0

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-0.rs
@@ -0,0 +1,14 @@
+
+impl Solution {
+    pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
+        fn dfs(i: usize, total: i32, nums: &Vec<i32>) -> i32 {
+            if (i == nums.len()) {
+                return total;
+            } else {
+                return dfs(i + 1, total ^ nums[i], nums) + dfs(i + 1, total, nums);
+            }
+        }
+
+        return dfs(0, 0, &nums);
+    }
+}
